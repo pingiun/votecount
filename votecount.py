ee1d7760c4f3d269752a911c69c0a952118e256c
Jelle Besseling <jelle@pingiun.com>
2022-11-27 18:17:18 +0100

    Fixes day of

--- votecount.py
+++ votecount.py
@@ -5,11 +5,12 @@ import csv
 import sys
 
 from collections import defaultdict
-from decimal import Decimal
+# from decimal import Decimal
+from fractions import Fraction
 
 vote_ballots = []
 
-with open("/Users/jelle/Downloads/STV template - Data.csv", 'r') as floor:
+with open(sys.argv[1], 'r') as floor:
     votereader = csv.reader(floor, delimiter=',', quotechar='"')
     names = next(votereader)
     for row in votereader:
@@ -53,11 +54,14 @@ class VoteCounter:
         self.names = names
         self.positions = positions
 
-        self.quota = math.floor((len(vote_ballots) / (positions + 1)) + 1)
+        self.quota = math.floor(len(vote_ballots) / (positions + 1)) + 1
         self.disqualified = set()
         self.elected = set()
-        self.counts = defaultdict(lambda: {"count": Decimal(0), "ballots": [Ballots()]})
+        self.counts = defaultdict(lambda: {"count": Fraction(0), "ballots": [Ballots()]})
         for i, ballot in enumerate(self.ballots):
+            print(len(ballot))
+            if len(ballot) == 0:
+                continue
             self.counts[ballot[0]]["count"] += 1
             self.counts[ballot[0]]["ballots"][0].add_ballot(ballot)
 
@@ -67,7 +71,7 @@ class VoteCounter:
             if person in self.disqualified and not person in self.elected:
                 print(f"{self.names[person]: <20}:      [ELIMINATED]")
                 continue
-            print(f"{self.names[person]: <20}: {count: >4}", end="")
+            print(f"{self.names[person]: <20}: {str(count): >4}", end="")
             if count >= self.quota:
                 print(" [ELECTED]", end="")
             if count > self.quota:
@@ -85,7 +89,7 @@ class VoteCounter:
                 self.elected.add(person)
                 self.disqualified.add(person)
             if count > self.quota:
-                surplus_amount = Decimal(count - self.quota)
+                surplus_amount = Fraction(count - self.quota)
                 surplus.append({"candidate": person, "surplus_amount": surplus_amount})
                 self.counts[person]["count"] -= surplus_amount
 
@@ -110,14 +114,18 @@ class VoteCounter:
             print()
             for person, nr in nr_ballots_increased.items():
                 print(f"Increasing {self.names[person]} by {nr}/{ballots_len}")
-                self.counts[person]["count"] += Decimal(nr) * Decimal(surp["surplus_amount"]) / Decimal(ballots_len)
+                self.counts[person]["count"] += Fraction(nr) * Fraction(surp["surplus_amount"]) / Fraction(ballots_len)
             print()
         return len(surplus) > 0
 
     def exclude_any_candidates(self):
         if len(self.elected) >= self.positions:
             return
-        lowest = sorted([(person, x["count"]) for person, x in self.counts.items() if person not in self.disqualified], key=lambda x: x[1])[0][0]
+        try:
+            lowest = sorted([(person, x["count"]) for person, x in self.counts.items() if person not in self.disqualified], key=lambda x: x[1])[0][0]
+        except IndexError:
+            # Er is geen laagste kandidaat meer
+            return False
         self.disqualified.add(lowest)
         print(f"Excluding: '{self.names[lowest]}', checking next preferences")
         for ballot_group in self.counts[lowest]["ballots"]:
@@ -138,8 +146,9 @@ class VoteCounter:
             print()
             for person, nr in nr_ballots_increased.items():
                 # print(f"Increasing {self.names[person]} by {nr}/{ballot_group.value}")
-                self.counts[person]["count"] += Decimal(nr) * Decimal(ballot_group.value) / Decimal(len(ballot_group))
+                self.counts[person]["count"] += Fraction(nr) * Fraction(ballot_group.value) / Fraction(len(ballot_group))
         print()
+        return True
 
     def _next_qualified_candidate(self, ballot):
         for elem in ballot:
@@ -150,10 +159,12 @@ class VoteCounter:
 positions = 3
 counter = VoteCounter(vote_ballots, names, positions)
 counter.print_status()
+print("QUOTA:", counter.quota)
 while len(counter.elected) < positions:
     surplus = counter.move_any_surplus()
     if surplus:
         counter.print_status()
     else:
-        counter.exclude_any_candidates()
+        if not counter.exclude_any_candidates():
+            break
         counter.print_status()
